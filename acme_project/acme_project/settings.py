5b941ac0c8b0242b0116cf5ad9020f2c05fa47c6
shipov.amr <shipov.max@icloud.com>
2025-12-22 22:54:51 +0300

    поменял чуток

--- acme_project/acme_project/settings.py
+++ acme_project/acme_project/settings.py
@@ -8,17 +8,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'birthday.apps.BirthdayConfig',
-    'pages.apps.PagesConfig',
-]
-
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
